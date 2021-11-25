3ecbdc0d91feec970a505f1c507ca6b8e8debe3b
DPHamdev <dphamill@gmail.com>
2021-11-25 23:47:04 +0000

    Add recipe functionality

--- app.py
+++ app.py
@@ -34,9 +34,31 @@ def recipes():
     return render_template("recipes.html")
 
 
-@app.route("/add_recipe")
+# Image Upload
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/add_recipe", methods=["GET", "POST"])
 def add_recipe():
-    return render_template("addrecipe.html")
+    if request.method == "POST":
+        recipe = {
+            "course_category": request.form.get("course_category"),
+            "recipe_name": request.form.get("recipe_name"),
+            "ingredients": request.form.get("ingredients"),
+            # "created_by": session["user"]
+        }
+        mongo.db.recipes.insert_one(recipe)
+
+        flash("Recipe Successfully Added")
+        return redirect( url_for("recipes") )
+
+    categories = mongo.db.recipeCategory.find().sort("course_category", 1)
+    return render_template("addrecipe.html", categories=categories)
 
 
 @app.route("/starters")
@@ -56,40 +78,31 @@ def dessert():
     recipe = list(mongo.db.recipes.find())
     return render_template("desserts.html", recipes=recipe)
 
-# Image Upload
 
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+# @app.route('/', methods=['POST'])
+# def upload_image():
+# 	if 'file' not in request.files:
+# 		flash('No file part')
+# 		return redirect(request.url)
+# 	file = request.files['file']
+# 	if file.filename == '':
+# 		flash('No image selected for uploading')
+# 		return redirect(request.url)
+# 	if file and allowed_file(file.filename):
+# 		filename = secure_filename(file.filename)
+# 		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+# 		#print('upload_image filename: ' + filename)
+# 		flash('Image successfully uploaded and displayed below')
+# 		return render_template('upload.html', filename=filename)
+# 	else:
+# 		flash('Allowed image types are -> png, jpg, jpeg, gif')
+# 		return redirect(request.url)
 
-def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-	
-@app.route('/')
-def upload_form():
-	return render_template('addrecipe.html')
 
-@app.route('/', methods=['POST'])
-def upload_image():
-	if 'file' not in request.files:
-		flash('No file part')
-		return redirect(request.url)
-	file = request.files['file']
-	if file.filename == '':
-		flash('No image selected for uploading')
-		return redirect('addrecipe.html')
-	if file and allowed_file(file.filename):
-		filename = secure_filename(file.filename)
-		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		#print('upload_image filename: ' + filename)
-		flash('Image successfully uploaded and displayed below')
-		return render_template('addrecipe.html', filename=filename)
-	else:
-		flash('Allowed image types are -> png, jpg, jpeg, gif')
-		return redirect('addrecipe.html')
-
-@app.route('/display/<filename>')
-def display_image(filename):
-	#print('display_image filename: ' + filename)
-	return redirect(url_for('static', filename='uploads/' + filename), code=301)
+# @app.route('/display/<filename>')
+# def display_image(filename):
+# 	#print('display_image filename: ' + filename)
+# 	return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 
 if __name__ == "__main__":
--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,33 @@
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "left"});
     $('.collapsible').collapsible();
+    $('select').formSelect();
 })
+
+// Image Upload display function
+
+const file = document.getElementById("file");
+const previewContainer = document.getElementById("imagePreview");
+const previewImage = previewContainer.querySelector(".image-preview__image");
+const previewDefaultText = previewContainer.querySelector(".imagepreview__default-text");
+
+file.addEventListener("change", function() {
+    const file = this.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        previewDefaultText.style.display = "none";
+        previewImage.style.display = "block";
+
+        reader.addEventListener("load", function() {
+            previewImage.setAttribute("src", this.result);
+        }); 
+
+        reader.readAsDataURL(file);
+    } else {
+        previewDefaultText.style.display = null;
+        previewImage.style.display = null;
+        previewImage.setAttribute("src", "");
+    }
+});
