2407b7e2cd76f624b8d2e16269113a536f499932
DPHamdev <dphamill@gmail.com>
2021-11-25 20:34:50 +0000

    Add image functionality

--- app.py
+++ app.py
@@ -4,15 +4,21 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+# Upload import
+from werkzeug.utils import secure_filename
 if os.path.exists("env.py"):
     import env
 
+UPLOAD_FOLDER = 'static/uploads/'
 
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
+# Upload authentication
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 mongo = PyMongo(app)
 
@@ -32,6 +38,7 @@ def recipes():
 def add_recipe():
     return render_template("addrecipe.html")
 
+
 @app.route("/starters")
 def starters():
     recipe = list(mongo.db.recipes.find())
@@ -48,6 +55,41 @@ def mCourse():
 def dessert():
     recipe = list(mongo.db.recipes.find())
     return render_template("desserts.html", recipes=recipe)
+
+# Image Upload
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	
+@app.route('/')
+def upload_form():
+	return render_template('addrecipe.html')
+
+@app.route('/', methods=['POST'])
+def upload_image():
+	if 'file' not in request.files:
+		flash('No file part')
+		return redirect(request.url)
+	file = request.files['file']
+	if file.filename == '':
+		flash('No image selected for uploading')
+		return redirect('addrecipe.html')
+	if file and allowed_file(file.filename):
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		#print('upload_image filename: ' + filename)
+		flash('Image successfully uploaded and displayed below')
+		return render_template('addrecipe.html', filename=filename)
+	else:
+		flash('Allowed image types are -> png, jpg, jpeg, gif')
+		return redirect('addrecipe.html')
+
+@app.route('/display/<filename>')
+def display_image(filename):
+	#print('display_image filename: ' + filename)
+	return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 
 if __name__ == "__main__":
