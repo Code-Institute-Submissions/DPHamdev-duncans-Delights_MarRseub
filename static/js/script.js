de816d60c7c6f39e57a6c14baf88b1038ba1609a
DPHamdev <dphamill@gmail.com>
2021-12-04 07:07:25 +0000

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -36,36 +36,3 @@ $(document).ready(function () {
         });
     }
 });
-
-
-
-    
-
-
-// Image Upload display function
-
-const file = document.getElementById("file");
-const previewContainer = document.getElementById("imagePreview");
-const previewImage = previewContainer.querySelector(".image-preview__image");
-const previewDefaultText = previewContainer.querySelector(".imagepreview__default-text");
-
-file.addEventListener("change", function() {
-    const file = this.files[0];
-
-    if (file) {
-        const reader = new FileReader();
-
-        previewDefaultText.style.display = "none";
-        previewImage.style.display = "block";
-
-        reader.addEventListener("load", function() {
-            previewImage.setAttribute("src", this.result);
-        }); 
-
-        reader.readAsDataURL(file);
-    } else {
-        previewDefaultText.style.display = null;
-        previewImage.style.display = null;
-        previewImage.setAttribute("src", "");
-    }
-});
